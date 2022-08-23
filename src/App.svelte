7dc590cffad9af9f10317c36d86836f6dce540e5
Philipp Sonntag <sonntag.philipp@outlook.de>
2022-08-23 02:59:45 +0200

    Updated page.

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,30 @@
 <script lang="ts">
-  import svelteLogo from './assets/profile_picture.png'
+  import profilePicture from './assets/profile_picture.png'
 </script>
 
-<main>
-  <div>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<main class="w-full h-full flex justify-center">
+  <div class="w-full xl:w-4/12 flex justify-evenly flex-col items-center">
+    <div class="flex items-center flex-col text-center">
+      <img src={profilePicture} class="rounded-full h-64 w-64 aspect-square overflow-hidden" alt="Svelte Logo" />
+      <h1 class="mt-4">Hello there 👋</h1>
+      <h1 class="mt-4">I'm <span class="italic">Philipp Sonntag</span>, a software developer for many things.</h1>
+      <h1 class="mt-4">
+        -
+      </h1>
+      <h1 class="mt-4">
+        Currently focused on <i>.NET, TypeScript / JavaScript, Angular</i> and <i>Web Components</i>.
+      </h1>
+    </div>
+    <div class="flex flex-col md:flex-row w-full justify-evenly items-center mb-14">
+      <a href="https://github.com/sonntag-philipp">GitHub</a>
+      <a href="https://stackoverflow.com/users/11581799">StackOverflow</a>
+      <a href="https://open.spotify.com/playlist/7kTZKAN5U7uyDPwVOm1Ec4?si=275e733f72b046d4">日本</a>
+    </div>
   </div>
-  <h1>Flyp.si</h1>
-
-  <div class="card">
-    Hallo :)
-  </div>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    border-radius: 50%;
-    height: 20rem;
-    padding: 1.5em;
-    will-change: filter;
+  a {
+    @apply underline my-2;
   }
 </style>
