8f40ec044108c976b28020f43fa3dc06cd30a00b
Philipp Sonntag <github@flyp.si>
2024-01-21 13:34:27 +0100

    Add Technologies component to page
    * Changed gradient to new color
    * Changed technologies

---
+++ src/lib/technologies.svelte
@@ -0,0 +1,50 @@
+<div class="w-full p-16 h-full max-w-screen-lg flex justify-center items-center">
+    <div>
+        <h1 class="text-2xl">My Technologies</h1>
+        <div class="flex flex-col sm:flex-row gap-4 py-4">
+            <p class="text-base">
+                <b class="underline underline-offset-2">Angular: </b> I've been working with Angular
+                for about 2 years now. I've worked on multiple projects, both frontend and backend, and
+                I'm familiar with the Angular ecosystem.
+            </p>
+            <p class="text-base">
+                <b class="underline underline-offset-2">SvelteKit: </b> I've been working with SvelteKit
+                for about 1 year now. I've worked on multiple projects, both frontend and backend, and
+                I'm familiar with the Svelte ecosystem.
+            </p>
+            <p class="text-base">
+                <b class="underline underline-offset-2">NodeJS: </b> I've been working with NodeJS for
+                about 2 years now. I've worked on multiple projects, both frontend and backend, and I'm
+                familiar with the NodeJS ecosystem.
+            </p>
+        </div>
+        <div class="flex flex-col sm:flex-row gap-4">
+            <p class="text-base">
+                <b class="underline underline-offset-2">Angular: </b> I've been working with Angular
+                for about 2 years now. I've worked on multiple projects, both frontend and backend, and
+                I'm familiar with the Angular ecosystem.
+            </p>
+            <p class="text-base">
+                <b class="underline underline-offset-2">SvelteKit: </b> I've been working with SvelteKit
+                for about 1 year now. I've worked on multiple projects, both frontend and backend, and
+                I'm familiar with the Svelte ecosystem.
+            </p>
+            <p class="text-base">
+                <b class="underline underline-offset-2">NodeJS: </b> I've been working with NodeJS for
+                about 2 years now. I've worked on multiple projects, both frontend and backend, and I'm
+                familiar with the NodeJS ecosystem.
+            </p>
+        </div>
+    </div>
+</div>
+
+<style>
+    * {
+        font-family: monospace;
+    }
+
+    p,
+    h1 {
+        color: white;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,26 @@
 <script>
     import Greeting from '$lib/greeting.svelte';
+    import Technologies from '$lib/technologies.svelte';
 </script>
 
 <div class="h-dvh w-full overflow-y-scroll snap-mandatory snap-y">
-    <div class="slides bg-slate-700">
+    <div class="slides">
         <Greeting />
     </div>
-    <div class="slides bg-blue-700">
-        <h1>My Technologies</h1>
+    <div class="slides">
+        <Technologies />
     </div>
     <div class="slides bg-teal-700">
-        <h1>Projects</h1>
+        <h1 class="text-white">Projects</h1>
+    </div>
+    <div class="slides bg-teal-700">
+        <h1 class="text-white">Education</h1>
     </div>
     <div class="slides bg-slate-700">
-        <h1>Education</h1>
+        <h1 class="text-white">Open Source</h1>
     </div>
-    <div class="slides bg-teal-700">
-        <h1>Open Source</h1>
-    </div>
-    <div class="slides bg-blue-700">
-        <h1>Contact</h1>
+    <div class="slides bg-amber-700">
+        <h1 class="text-white">Contact</h1>
     </div>
 </div>
 
@@ -30,6 +31,7 @@
     }
 
     .slides {
+        @apply bg-gradient-to-r from-slate-700 via-cyan-900 to-green-700;
         @apply flex flex-col items-center justify-center w-full h-dvh gap-4 snap-start;
     }
 </style>
